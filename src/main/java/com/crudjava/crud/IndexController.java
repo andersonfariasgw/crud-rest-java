0637bb2bb0a831dc27fcd01200e19463df95315f
andersonEspana <andersonfarias@gwsistemas.com.br>
2020-02-04 18:10:14 -0300

    colocando index controlador para receber as requisições da minha aplicação front

---
+++ src/main/java/com/crudjava/crud/IndexController.java
@@ -0,0 +1,14 @@
+package com.crudjava.crud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping(value = {"/", "/dashboard", "/intranet"})
+    public String index() {
+        return "index.html";
+    }
+}
